61df8703826e70c7987c3a6f05698212a3243de8
Anastasiya <canastasia939@gmail.com>
2021-10-13 02:54:49 +0300

    First commit

---
+++ ConsoleApp1210/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string message = "Hello, Server";
+            
+
+
+            var answear = GetRequest("http://localhost:51803/Home/", message).Result;
+
+
+
+            Console.WriteLine(answear.ToString());
+            Console.ReadLine();
+
+
+        }
+        static async Task<string> GetRequest (string adress, string message)
+        {
+            HttpClient client = new HttpClient();
+            
+            
+                Uri uri = new Uri(adress);
+                StringContent content = new StringContent(message);
+
+
+            var request = await client.PostAsync(adress, content);
+            var responce = await request.Content.ReadAsStringAsync();
+            return responce;
+
+        }
+    }
+}
---
+++ Server7/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+
+
+
+namespace Server7.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpPost]
+        public string Messenger()
+        {
+
+            using (StreamReader reader = new StreamReader(HttpContext.Request.BodyReader.AsStream()))
+
+            {
+                var message = reader.ReadToEnd();
+                return message + " Hi, Client! ";
+
+                             
+            }
+
+
+        }
+
+
+    }
+}
